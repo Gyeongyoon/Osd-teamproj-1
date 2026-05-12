760f439e75c6561388abd8d1847ff940ca2d0fd2
sihyun <username@Hyun.localdomain>
2026-05-12 20:23:32 +0900

    mistake

--- xv6-riscv/user/user.h
+++ xv6-riscv/user/user.h
@@ -24,7 +24,7 @@ int getpid(void);
 char* sys_sbrk(int,int);
 int pause(int);
 int uptime(void);
-void* mmap(void*, int, int, int, int, int);
+void* mmap(uint64, int, int, int, int, int);
 int   munmap(uint64);
 int   freemem(void);
 
