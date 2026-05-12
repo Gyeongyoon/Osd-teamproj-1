970bce58fc63e0e9795c64b296128c01144804f5
Gyeongyoon <gracekim6531@naver.com>
2026-05-12 19:49:27 +0900

    feat: register mmap/munmap/freemem syscall numbers and declarations
    - Add SYS_mmap=27, SYS_munmap=28, SYS_freemem=29 to syscall.h
    - Register syscall function pointers in syscall.c
    - Add usys.pl entries for mmap, munmap, freemem
    - Add user-level declarations in user.h

--- xv6-riscv/user/user.h
+++ xv6-riscv/user/user.h
@@ -24,6 +24,9 @@ int getpid(void);
 char* sys_sbrk(int,int);
 int pause(int);
 int uptime(void);
+void* mmap(void*, int, int, int, int, int);
+int   munmap(uint64);
+int   freemem(void);
 
 // ulib.c
 int stat(const char*, struct stat*);
