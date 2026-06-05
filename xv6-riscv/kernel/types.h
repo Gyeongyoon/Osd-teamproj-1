acfca5ef3a9c9981999cefd794d7fbf546ab8fe8
sihyun <username@Hyun.localdomain>
2026-06-05 18:45:35 +0900

    PA4 setup WIP: struct page, PTE macros, skeleton files

--- xv6-riscv/kernel/types.h
+++ xv6-riscv/kernel/types.h
@@ -8,3 +8,12 @@ typedef unsigned int  uint32;
 typedef unsigned long uint64;
 
 typedef uint64 pde_t;
+// PA4: one node per physical frame, linked into the global LRU list.
+#ifndef PA4_STRUCT_PAGE
+#define PA4_STRUCT_PAGE
+struct page {
+  struct page *next, *prev;
+  uint64       pagetable;   // owning user page table (0 = not on list)
+  uint64       vaddr;       // user VA this frame is mapped to
+};
+#endif
